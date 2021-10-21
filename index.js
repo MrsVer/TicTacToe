8d8f79cf70eb4b9794a1b3a1ef1daeeadd7f5110
Zsuzsanna Ver <56695526+MrsVer@users.noreply.github.com>
2021-10-21 15:38:04 +0100

    style changes

--- index.js
+++ index.js
@@ -53,10 +53,10 @@ window.addEventListener('DOMContentLoaded', () => {
     const announce = (type) => {
         switch(type){
             case PLAYERO_WON:
-                announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
+                announcer.innerHTML = 'Player <span class="playerO"> "O" </span> Won';
                 break;
             case PLAYERX_WON:
-                announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
+                announcer.innerHTML = 'Player <span class="playerX"> "X" </span> Won';
                 break;
             case DRAW:
                 announcer.innerText = 'Draw';
